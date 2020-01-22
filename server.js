56776c2b9008c0af7466356f066095f3f1ed1f8a
neuron909 <kaushikrb909@gmail.com>
2020-01-22 20:43:35 +0530

    initialization moved to init.js

--- server.js
+++ server.js
@@ -1,25 +1,9 @@
-global.SERVER_PORT = 9999
-const dbUser = 'user'
-const dbPass = 'user123'
-const dbName = 'test'
-
-
-
-global.DB = {
-    USER : dbUser,
-    PASSWORD : dbPass,
-    URL : `mongodb+srv://${dbUser}:${dbPass}@cluster0-hfbmi.mongodb.net/${dbName}?retryWrites=true&w=majority`
-}
-
-//db connection
-require('./connection').connectDB()
+require('./init')()
 
 //modules for express
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-
-
 
 app.use(bodyParser.urlencoded({extended : true}))
 app.use(bodyParser.json())
