ebf58e49d390fa80d03054e23bcc34de233742cc
neuron909 <kaushikrb909@gmail.com>
2020-01-22 17:29:04 +0530

    specify DB_URL in global var

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-global.port = 9999
+global.SERVER_PORT = 9999
+global.DB_URL = 'mongodb+srv://user:user123@cluster0-hfbmi.mongodb.net/test?retryWrites=true&w=majority'
 
 //db connection
 require('./connection').connectDB()
@@ -16,6 +17,6 @@ app.use(bodyParser.json())
 //will automatically search for index.js in './routes' folder
 app.use(require('./routes'))
 
-app.listen(global.port , ()  =>  {
+app.listen(global.PORT , ()  =>  {
     console.log('Server Running...')
 })
