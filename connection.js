f1b04aef5cda8d01e7b852776eaf4a6952f08a3a
neuron909 <kaushikrb909@gmail.com>
2020-01-23 14:49:12 +0530

    Register schema to models here

--- connection.js
+++ connection.js
@@ -1,10 +1,22 @@
 const mongoose = require('mongoose')
+//fixex all deprecation warnings
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+
+
+//Register schema to models here
+
+
 
 module.exports.connectDB = () => {
-    try{
-        mongoose.connect( global.DB.URL , { useNewUrlParser: true ,  useUnifiedTopology: true });
-        console.log('connected to DB')
-    }catch(err){
-        console.log(err)
-    }
+    mongoose.connect( process.env.DB_URL,(err) =>{
+        if(err){
+            console.log('Not Connected to DB')
+            throw err
+        }else{
+            console.log('Connected to DB')
+        }
+    });
 }
