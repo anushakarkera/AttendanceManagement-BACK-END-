18ca1dad263d76c136ae0f2b849244a9cb6bda2d
3shool <tsherigar@gmail.com>
2020-01-23 15:46:48 +0530

    added login

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,5 +1,22 @@
-module.exports.login = (req,res,next) =>{
-    res.send('Inside Login')
+const User = require('../models/user.model')
+
+
+
+module.exports.login = async(req, res) => {
+    //Login a registered user
+    try {
+        const { email, password } = req.body
+        const user = await User.findByCredentials(email, password)
+        if (!user) {
+            return res.status(401).send({error: 'Login failed! Check authentication credentials'})
+        }
+        const token = await user.generateAuthToken()
+        //console.log(token);
+        res.status(200).json({ok:true,_id:user.id,token:token});
+        //res.send(a);
+    } catch (error) {
+        res.status(400).send(error)
+    }
 
 }
 
