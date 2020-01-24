51e45ae11d3628045cedc585a47e4765d931fc72
Sha-Kumar <shashankneeded@gmail.com>
2020-01-24 17:41:26 +0530

    ooo

--- models/user.model.js
+++ models/user.model.js
@@ -1,70 +1,66 @@
-//user schema
-const mongoose = require('mongoose')
-const validator = require('validator')
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
-// const Math = require('')
+// //user schema
+// const mongoose = require('mongoose')
+// const validator = require('validator')
+// const bcrypt = require('bcryptjs')
+// const jwt = require('jsonwebtoken')
+// // const Math = require('')
 
-var userSchema = new mongoose.Schema({
-    fullName: {
-        type: String,
-        trim: true
-    },
-    email: {
-        type: String,
-        unique: true
-    },
-    password: {
-        type: String
-        },
-    phone: {
-        type: String
-    },
-    gender: {
-        type: String
-    },
-    city: {
-        type: String
-    },
-    tokens: [{
-        token: {
-            type: String,
-            required: true
-        }
-    }]
-});
-userSchema.pre('save', async function (next) {
-    // Hash the password before saving the user model
-    const user = this
-    if (user.isModified('password')) {
-        user.password = await bcrypt.hash(user.password, Math.random())
-    }
-    next();
-})
+// var userSchema = new mongoose.Schema({
+//     fullName: {
+//         type: String,
+//         trim: true
+//     },
+//     email: {
+//         type: String,
+//         unique: true
+//     },
+//     password: {
+//         type: String
+//         },
+//     phone: {
+//         type: String
+//     },
+//     gender: {
+//         type: String
+//     },
+//     city: {
+//         type: String
+//     },
+//     tokens: [{
+//         token: {
+//             type: String,
+//             required: true
+//         }
+//     }]
+// });
+// userSchema.pre('save', async function (next) {
+//     // Hash the password before saving the user model
+//     const user = this
+//     if (user.isModified('password')) {
+//         user.password = await bcrypt.hash(user.password, Math.random())
+//     }
+//     next();
+// })
 
-userSchema.methods.generateAuthToken = async function() {
-    const user = this
-    const token = jwt.sign({_id: user._id}, process.env.JWT_KEY,{expiresIn: '1d'});
-    user.tokens = user.tokens.concat({token})
-    await user.save()
-    return token
-}
+// userSchema.methods.generateAuthToken = async function() {
+//     const user = this
+//     const token = jwt.sign({_id: user._id}, process.env.JWT_KEY,{expiresIn: '1d'});
+//     user.tokens = user.tokens.concat({token})
+//     await user.save()
+//     return token
+// }
 
-userSchema.statics.findByCredentials = async (email, password) => {
-    const user = await User.findOne({ email} )
-    if (!user) {
-        throw new Error({ error: 'Invalid login credentials' })
-    }
-    const isPasswordMatch = await bcrypt.compare(password, user.password)
-    if (!isPasswordMatch) {
-        throw new Error({ error: 'Invalid login credentials' })
-    }
-    return user
-}
+// userSchema.statics.findByCredentials = async (email, password) => {
+//     const user = await User.findOne({ email} )
+//     if (!user) {
+//         throw new Error({ error: 'Invalid login credentials' })
+//     }
+//     const isPasswordMatch = await bcrypt.compare(password, user.password)
+//     if (!isPasswordMatch) {
+//         throw new Error({ error: 'Invalid login credentials' })
+//     }
+//     return user
+// }
 
-const abcd = 10;
-console.log(abcd);
-
-
-const User = mongoose.model('User', userSchema);
-module.exports  = User;
+// const User = mongoose.model('User', userSchema);
+// module.exports  = User;
