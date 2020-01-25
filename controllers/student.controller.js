72b8aefb1d854ee19af6f38ce1e61937f6cb6661
Sha-Kumar <shashankneeded@gmail.com>
2020-01-25 21:48:37 +0530

    I Changed To its Standard Structure

--- controllers/student.controller.js
+++ controllers/student.controller.js
@@ -1,18 +1,22 @@
 const  mongoose = require('mongoose');
 const Student = require('../models/student.model')
 
+const Handler = require('./handler');
 
 module.exports.student = async (req,res)=>{
 var b = await Student.find({});
-if(b)
-        res.send(b);
+if(b){
+        Handler.resultHandler(200, "OK", "Student Details Found", b, res);   
+        //res.send(b);
+}
 else 
     {
-        var errr={};
-        errr.code=404
-        errr.status='Not found';
-        errr.message='Students details not found';
-        res.send(errr);
+        Handler.resultHandler(404, "Not Found", "Student Details Not Found", "error", res);
+        // var errr={};
+        // errr.code=404
+        // errr.status='Not found';
+        // errr.message='Students details not found';
+        // res.send(errr);
 
     }      
 }
--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,21 +1,21 @@
 const User = require('../models/user.model');
 
-
+const Handler = require('./handler');
 
 module.exports.login = async(req, res) => {
     //Login a registered user
     try {
-        const { email, password } = req.body
-        const user = await User.findByCredentials(email, password)
+        const { email, password } = req.body;
+        const user = await User.findByCredentials(email, password);
         if (!user) {
-            return res.status(401).send({error: 'Login failed! Check authentication credentials'})
+            return Handler.resultHandler(401, "Error", "Username is wrong!!! Login Failed.", "error", res);//res.status(401).send({error: 'Login failed! Check authentication credentials'});
         }
-        const token = await user.generateAuthToken()
-        //console.log(token);
-        res.status(200).json({ok:true,_id:user.id,token:token});
-        //res.send(a);
+        const token = await user.generateAuthToken();
+
+        var doc = { ok : true , _id : user._id, token : token };
+        Handler.resultHandler(200, "OK", "Successful Login", doc, res);//res.status(200).json({ok:true,_id:user.id,token:token});
     } catch (error) {
-        res.status(400).send(error)
+        Handler.resultHandler(400, "Error", "Login Failed!!! Username or Password is Incorrect.", "error", res);//res.status(400).send(error);
     }
 
 }
@@ -30,25 +30,27 @@ module.exports.signup = (req,res,next) =>{
     user.city = req.body.city;
     user.save((err, doc) => {
         if (!err) {
-            var result = {}
+            Handler.resultHandler(200, "OK", "Successfully Registered", doc, res);
+            // var result = {};
             
-            result.responseCode = 200;
-            result.status = "OK";
-            result.message = "Successfully registered"
-            result.data = doc;
-
-            res.send(result);
+            // result.responseCode = 200;
+            // result.status = "OK";
+            // result.message = "Successfully registered"
+            // result.data = doc;
+            //res.send(result);
 
         }
         
         else
         {
             if(err.code === 11000){
-             var error = {};
-             error.code = 409;
-             error.status = 'conflict';
-             error.message = 'duplicate email';
-              res.send(error);
+            
+                Handler.resultHandler(409, "CONFLICT", "Duplicate Email", "error", res);
+            //  var error = {};
+            //  error.code = 409;
+            //  error.status = 'conflict';
+            //  error.message = 'duplicate email';
+            //   res.send(error);
             }
             else
                 return next(err);
