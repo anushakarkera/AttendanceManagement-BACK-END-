18658835412db8fe9dae3d5486bf5eae69065e0d
neuron909 <kaushikrb909@gmail.com>
2020-01-26 12:01:56 +0530

    updated Response

--- response.js
+++ response.js
@@ -1,48 +1,76 @@
 module.exports = class Response{
-    constructor(res){
-        this.res = res;
-        this.code=200;
-    }
-    setError(err){
-        this.error = err;
-        return this;
-    }
-    setCode(code){
-        this.code = code;
-        return this;
-    }
-    setMessage(msg){
-        this.message = msg;
-        return this;
-    }
-    setData(data){
-        this.data = data;
-        return  this;
-    }
-    setStatus(sts){
-        this.status = sts;
-        return this;
-    }
-    async send(){
-        this.res.status(this.code).send(await this.prepareStructure());
-    }
-    prepareStructure(){
-        //will add the fields which are 'set' to 'structure' 
-        let structure = {};
-        if(this.status) structure.resStatus = this.status;
-        if(this.message) structure.resMessage = this.message;
-        if(this.data) structure.data = this.data;
-        if(this.error) structure.error = this.error;
-        return structure;
+    constructor(resCode){
+    	let res = {Status : 'SUCCESS'};
+
+        this.setMessage = (msg)  => { res.Message = msg;    return this;    }
+        this.setData    = (data) => { res.Data = data;      return this; 	}
+        this.setError   = (err)  => { res.Error = err;      return this; 	}
+        
+        this.send = (resObj) => {
+            if(resCode === 200)
+                resObj.status(resCode).send(res);
+            else{
+                res.Status = 'FAILED';
+                if(defaultResponse[resCode] === undefined)
+                    resObj.status(404).send(defaultResponse[404]);
+                else
+                    resObj.status(resCode).send(Object.assign(res,defaultResponse[resCode]));
+            }
+        }
     }
 }
 
-//by default respose code will be set to 200 (assuming postive response)
+/*
+-- add negative response here using response code as a key --
+*/
+let defaultResponse = {
+    404 :  {
+        Message : 'Unknown Request',
+        Error  : 'Default Response Not Defined'
+    },
+
+    401 : {
+        Message : 'Incorrect Email or Password',
+        Error   : 'Incorrect Credential : User Authentication Unsuccessful'
+    },
+
+    409 : {
+        Message : 'User Already Exist',
+        Error   : 'Email Conflict : User Registration Unsuccessful'
+    }
+}
 
 /*
-let r = new Response(res)    //pass res object to constructor
-r.setMessage('message to client')
-r.setStatus('Success')
-//we can also chain the methods
-r.setData(clientData).send();
+-- Available Methods --
+setMessage()
+setData()
+setError()
+send(res)  //pass 'res' object 
+
+
+-- How to Use --
+
+Sending Successful Response       (pass 200 as response code)
+
+let response = new Response( responseCode );       
+response.setMessage('Response Message');
+response.setData (ResponseData);
+response.send(res);     
+
+        -OR-
+
+new Response( responseCode )                       
+    .setMessage('Response Message')
+    .setData (ResponseData)
+    .send(res);
+
+
+
+Sending Negative Response   (pass a standard code as responseCode)
+
+    new Response( responseCode )
+        .send(res);
+
+Note : Define response content in defaultResponse { } (defined above)
+        
 */
