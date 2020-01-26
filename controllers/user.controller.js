3174a965521740e4539552f00f272360ca86e6db
neuron909 <kaushikrb909@gmail.com>
2020-01-26 12:02:16 +0530

    added Response

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,66 +2,36 @@ const User = require('../models/user.model');
 
 const Handler = require('./handler');
 // const User = require('mongoose').model('User')
-// const Response = require('../response');
+const Response = require('../response');
 
 
 module.exports.login = async(req, res) => {
-    //Login a registered user
     try {
         const { email, password } = req.body;
-        const user = await User.findByCredentials(email, password);
-        if (!user) {
-            return Handler.resultHandler(401, "Error", "Username is wrong!!! Login Failed.", "error", res);//res.status(401).send({error: 'Login failed! Check authentication credentials'});
-        }
+        const user = await User.findByCredentials(email, password); //throws exception if credential doesnt match
         const token = await user.generateAuthToken();
-
-        var doc = { ok : true , _id : user._id, token : token };
-        Handler.resultHandler(200, "OK", "Successful Login", doc, res);//res.status(200).json({ok:true,_id:user.id,token:token});
+        const resData = {userID : user._id, userToken : token };
+        new Response(200)
+            .setMessage('User Login Authorised')
+            .setData(resData)
+            .send(res);
     } catch (error) {
-        Handler.resultHandler(400, "Error", "Login Failed!!! Username or Password is Incorrect.", "error", res);//res.status(400).send(error);
-        // let r = new Response(res);
-        // r.setCode(400).setError('Incorrect Email or Pass').setMessage('Incorrect email or Pass');
-        // r.setStatus('unknown').send();
-        // res.status(400).send(error)
+       new Response(401).send(res);
     }
 
 }
 
 module.exports.signup = (req,res,next) =>{
     var user = new User();
-    user.fullName = req.body.fullName;
-    user.email = req.body.email;
-    user.password = req.body.password;
-    user.phone = req.body.phone;
-    user.gender = req.body.gender;
-    user.city = req.body.city;
+    Object.assign(user,req.body);   // assigns data from req.body to user model
     user.save((err, doc) => {
-        if (!err) {
-            Handler.resultHandler(200, "OK", "Successfully Registered", doc, res);
-            // var result = {};
-            
-            // result.responseCode = 200;
-            // result.status = "OK";
-            // result.message = "Successfully registered"
-            // result.data = doc;
-            //res.send(result);
-
-        }
-        
+        if (!err) 
+            new Response(200)
+                .setMessage('User Registration Successful')
+                .setData(doc)
+                .send(res);
         else
-        {
-            if(err.code === 11000){
-            
-                Handler.resultHandler(409, "CONFLICT", "Duplicate Email", "error", res);
-            //  var error = {};
-            //  error.code = 409;
-            //  error.status = 'conflict';
-            //  error.message = 'duplicate email';
-            //   res.send(error);
-            }
-            else
-                return next(err);
-        }
+            new Response(409).send(res);
     });
 }
 module.exports.profileupdate=(req,res,next)=>{
