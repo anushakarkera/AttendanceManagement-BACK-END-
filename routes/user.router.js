bb9b867c74781853a7d8351a5095fb3c5b141464
neuron909 <kaushikrb909@gmail.com>
2020-01-27 22:59:56 +0530

    added timeTable

--- routes/user.router.js
+++ routes/user.router.js
@@ -1,13 +1,10 @@
-const express = require('express')
-const router = express.Router()
+const router = require('express').Router();
 const auth  = require('../middleware/auth')
-//const user = require('../models/user.model')
-
-
 const userController = require('../controllers/user.controller')
 
 router.post('/login',userController.login);
 router.post('/signup',userController.signup);
-router.put('/profileupdate/:id',userController.profileupdate)
+router.put('/profileupdate/:id',auth,userController.profileupdate);
+router.get('/timeTable/:user_id',userController.timeTable);
 
 module.exports = router;
