7bb707c7f7ccf95fb05795194b4224ffd5b10a57
neuron909 <kaushikrb909@gmail.com>
2020-01-28 11:06:40 +0530

    added classSubject_id to the path

--- routes/student.router.js
+++ routes/student.router.js
@@ -1,10 +1,7 @@
-const express = require('express');
-const router = express.Router();
-const auth  = require('../middleware/auth')
-
-const ctrlStu = require('../controllers/student.controller');
-// const studentController = require('../controllers/student.controller')
+const router = require('express').Router();
+// const auth  = require('../middleware/auth');
+const studentController = require('../controllers/student.controller')
 
 //student routing
-router.get('/list',auth,ctrlStu.student);
+router.get('/list:classSubject_id',studentController.list);
 module.exports = router;
