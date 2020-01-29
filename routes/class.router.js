031dd45ffba1bf49f20e824fd495f4ce721f9282
neuron909 <kaushikrb909@gmail.com>
2020-01-29 14:04:50 +0530

    added attendance (partial module)

--- routes/class.router.js
+++ routes/class.router.js
@@ -1,7 +1,8 @@
-const express = require('express');
-const router = express.Router();
-// const userController = require('../controllers/class.controller')
+const router = require('express').Router();
+const classController = require('../controllers/class.controller')
 
 //class Routing
+router.post('/attendance',classController.addAttendance);
+
 
 module.exports = router;
