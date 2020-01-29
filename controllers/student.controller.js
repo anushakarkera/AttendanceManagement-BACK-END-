dff14735b0ebb1866198f13944f831168b9fd3ee
Sha-Kumar <shashankneeded@gmail.com>
2020-01-29 17:49:05 +0530

    student list api updated

--- controllers/student.controller.js
+++ controllers/student.controller.js
@@ -1,16 +1,21 @@
 const Student = require('../models/student.model');
+const classSubject = require('../models/classSubject.model');
+const mongoose = require('mongoose');
+
 const Response = require('../response.js');
+// const Mongoose = mongoose.Schema;
 
 module.exports.list = async (req,res)=>{
-    Student.find({})
-        .then(values =>{
-            new Response(200).setData(values).send(res);
-        },reason => {
-            new Response(404).send(res);
-        })
+    await classSubject.findOne({_id : req.query.id}).then(values => {
+
+        var arraylist = [];
+        Student.find({class_id : values.class_id})
+        .then(value =>{
+               res.send(value);
+           },reason => {
+               new Response(404).send(res);
+           });        
+    }, error => {
+        res.send("error");
+    });
 }
-   
-
-
-    
-
