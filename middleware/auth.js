cf9dce9bf306a9d86b0ec2a21da6966f1251c90d
Sha-Kumar <shashankneeded@gmail.com>
2020-01-31 16:33:28 +0530

    update in middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,8 +2,7 @@ const jwt = require('jsonwebtoken')
 const User = require('../models/user.model')
 const handler = require('../controllers/handler')
 const auth = async(req, res, next) => {
-    if(req.url === '/user/login' || req.url === '/user/signup')
-    {
+    if(req.url === '/user/login' || req.url === '/user/signup' || req.url === '/user/forgotpassword' || req.url === '/user/newpassword'){
         next();
         return true;
     }
@@ -11,16 +10,14 @@ const auth = async(req, res, next) => {
     {
     try {
         
-        if(req.url === '/user/login' || req.url === '/user/signup' || req.url === '/user/forgotpassword' || req.url === '/user/newpassword'){
-            next();
-            return true;
-        }
+        
     const token = req.header('Authorization').replace('Bearer ', '');
     //console.log(req.header('Authorization'))
     const data = jwt.verify(token, process.env.JWT_KEY);
     //console.log(data);
         const user = await User.findOne({ _id: data._id, token: token });
         if (!user) {
+            console.log('yes')
             throw new Error();
         }
         req.userID= user._id;
