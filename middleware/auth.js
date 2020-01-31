39c0b5f035c3a8aeabbdf0539c8f6c4ab8347cb0
3shool <tsherigar@gmail.com>
2020-01-31 17:39:12 +0530

    auth update

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,19 +2,9 @@ const jwt = require('jsonwebtoken')
 const User = require('../models/user.model')
 const handler = require('../controllers/handler')
 const auth = async(req, res, next) => {
-    if(req.url === '/user/login' || req.url === '/user/signup' || req.url === '/user/forgotpassword' || req.url === '/user/newpassword'){
-        next();
-        return true;
-    }
-    else
-    {
     try {
-        
-        
-    const token = req.header('Authorization').replace('Bearer ', '');
-    //console.log(req.header('Authorization'))
-    const data = jwt.verify(token, process.env.JWT_KEY);
-    //console.log(data);
+        const token = req.header('Authorization').replace('Bearer ', '');
+        const data = jwt.verify(token, process.env.JWT_KEY);
         const user = await User.findOne({ _id: data._id, token: token });
         if (!user) {
             console.log('yes')
@@ -26,9 +16,13 @@ const auth = async(req, res, next) => {
         next();
         return true;
     } catch (error) {
-        handler.resultHandler(401,'Error','Not authorized to access this resource','error',res)
+        if(req.url === '/user/login' || req.url === '/user/signup' || req.url === '/user/forgotpassword' || req.url === '/user/newpassword'){
+            next();
+            // return true;
+        }else{
+            handler.resultHandler(401,'Error','Not authorized to access this resource','error',res)
+        }
+        
     }
-
-}
 }
 module.exports = auth
