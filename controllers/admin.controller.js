8048ed455a695322a942bd236639183d1cd930fc
SHRITHIP <shrithi99@gmail.com>
2020-02-10 21:58:26 +0530

    deleteuser

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -1,12 +1,15 @@
 const User = require('../models/user.model');
 const Response=require('../response')
 module.exports.Deleteuser=async(req,res,next)=>{
-    var id=req.body.email
-    User.findOneAndDelete({email:id})
-    .then(value => {
-        new Response(200).send(res);
+    var userEmail=req.body.email;
+        User.findOneAndDelete({email:userEmail},{email:true})
+        .then(value => {
+            if (!value) throw ('email not found')
+            new Response(200).send(res);
         console.log("Deleted Successfully!")
-    })
+    }/*,reason => {
+        new Response(404).send(res)
+    }*/)
     .catch(err => {
         new Response(404).send(res);
     })
